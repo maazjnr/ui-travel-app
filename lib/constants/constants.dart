9ea365a61fff0837105de805ebec611aef42f04c
maazjnr <goalmaaz@gmail.com>
2023-01-24 13:28:08 +0100

    first 001

---
+++ lib/constants/constants.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+/// Colors
+const Color primaryDark = Color.fromARGB(255, 3, 74, 110);
+const Color primaryLight = Color.fromARGB(255, 115, 204, 233);
+
+const Color secondaryDark = Color(0xFF507DFF);
+const Color secondaryLight = Color(0xFF60A6FF);
+
+const Color fontDark = Color.fromARGB(255, 26, 29, 35);
+const Color fontLight = Color(0xFFB3C7E0);
+
+const Color accent = Color(0xFF8A4D52);
+const Color background = Color(0xFFF1F2F5);
+
+/// dimensions
+const double defaultSpacing = 16.0;
+const double defaultRadius = 12.0;
+
+const double fontSizeHeading = 18.0;
+const double fontSizeTitle = 16.0;
+const double fontSizeBody = 13.0;
---
+++ lib/screens/getstart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class GetStartedScreen extends StatefulWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  State<GetStartedScreen> createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
