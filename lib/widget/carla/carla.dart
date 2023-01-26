0ac120095cedaa765a116df65b5780ae98c8194a
maazjnr <goalmaaz@gmail.com>
2023-01-26 02:43:37 +0100

    ggg

---
+++ lib/widget/carla/carla.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Carla extends StatefulWidget {
+  const Carla({super.key});
+
+  @override
+  State<Carla> createState() => _CarlaState();
+}
+
+class _CarlaState extends State<Carla> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
